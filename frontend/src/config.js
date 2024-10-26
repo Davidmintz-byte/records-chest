968ba2cef886ea1534e29c6e7345bd109b5118c4
David Mintz <davidmintz3@gmail.com>
2024-10-26 18:08:44 +0300

    Update env config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,10 @@
-const API_URL = 'https://records-chest.onrender.com';
-console.log('API URL:', API_URL);
+console.log('Current environment:', process.env.NODE_ENV);
+console.log('Current API URL:', process.env.REACT_APP_API_URL);
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+if (!API_URL) {
+    console.error('API URL not configured!');
+}
 
 export { API_URL };
